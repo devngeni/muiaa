2d703e47ee7b8020c197c93011ffe18c48d7c39e
collinskoech11 <collinskoechck34@gmail.com>
2024-01-01 09:41:18 +0300

    footer update

--- frontend/src/StyledComponents/Footer.ts
+++ frontend/src/StyledComponents/Footer.ts
@@ -21,3 +21,25 @@ export const FooterText = styled(Typography)({
 export const FooterHeader = styled(FooterText)({
     fontWeight: "bold",
 })
+
+export const MainFooterBox = styled(Box)({
+  width: "100vw",
+  height: "auto",
+  backgroundColor: "#14263E",
+  minHeight: "5vh",
+  paddingTop: "70px",
+  paddingBottom: "70px",
+});
+
+export const FooterTextBox = styled(Typography)({
+  color: "#fff",
+  fontSize: "16px",
+  fontWeight: "300",
+  textAlign: "left",
+  paddingLeft: "30px",
+  paddingBottom: "10px",
+  fontFamily: "'Inter'",
+});
+export const FooterHeaderBox = styled(FooterText)({
+    fontWeight: "bold",
+})
