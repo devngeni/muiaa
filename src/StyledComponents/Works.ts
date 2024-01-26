5d5c95207662d18d36c313a519e602bca9ac6db9
collinskoech11 <collinskoechck34@gmail.com>
2024-01-26 09:37:26 +0300

    how it works

--- src/StyledComponents/Works.ts
+++ src/StyledComponents/Works.ts
@@ -1,11 +1,11 @@
-import { Box, styled } from "@mui/material";
+import { Box, Grid, styled } from "@mui/material";
 
 
 export const WorksMainHero = styled(Box)({
     width:"100vw",
     height:"auto",
     minHeight:"30vh",
-    backgroundImage:"url(assets/WorksHerobg.svg)",
+    backgroundImage:"url(assets/works-hero.png)",
     backgroundSize:"cover",
     backgroundPosition:"center",
     backgroundRepeat:"no-repeat",
@@ -14,9 +14,38 @@ export const WorksMainHero = styled(Box)({
 })
 export const BgOverlay = styled(Box)({
     width:"100%",
-    height:"30vh",
-    backgroundColor:"rgba(0, 53, 101, 0.8)",
+    height:"auto",
     display:"flex",
     alignItems:"center",
 })
-
+export const PlatformContainer = styled(Grid)({
+    width:"100%",
+    display:"flex",
+    background:"#14263E",
+})
+export const PLatformItem = styled(Grid)({
+})
+export const WhiteBox  = styled(Box)({
+    width:"100%",
+    maxWidth:"800px",
+    height:"400px",
+    background:"#fff",
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+    padding:"50px",
+    borderTopRightRadius:"150px",
+})
+export const Part = styled(Box)({
+  width: "100%",
+  height: "auto",
+  minHeight: "35vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundImage: "url(assets/works-hero.png)",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+});
--- src/pages/how-it-works.tsx
+++ src/pages/how-it-works.tsx
@@ -1,23 +1,181 @@
 import Footer from '@/Components/Footer';
 import Navbar from '@/Components/Navbar';
-import { BgOverlay, WorksMainHero } from '@/StyledComponents/Works';
-import { Box } from '@mui/material';
+import { OrangeButton, UnparalleledMainText, UnparalleledSubText } from '@/StyledComponents/HomeHero';
+import {
+  BgOverlay,
+  Part,
+  PlatformContainer,
+  PLatformItem,
+  WhiteBox,
+  WorksMainHero,
+} from "@/StyledComponents/Works";
+import { Box, Grid, Typography } from '@mui/material';
 import React from 'react'
 
 function HotItWorks() {
   return (
     <>
-    <Navbar />
-    <WorksMainHero>
-      <BgOverlay >
-        <Box sx={{textAlign:"center", width:"100%"}}>
-          <h1>How It Works</h1>
+      <Navbar />
+      <WorksMainHero>
+        <BgOverlay>
+          <Box sx={{ textAlign: "center", width: "100%" }}>
+            <Typography
+              sx={{
+                color: "#ffff",
+                fontWeight: "900",
+                fontSize: "60px",
+                maxWidth: "1200px",
+                margin: "auto",
+                mt: "100px",
+                lineHeight: "",
+                letterSpacing: "-3px",
+              }}
+            >
+              Reliable supply for all your dairy products, across the border
+            </Typography>
+            <OrangeButton
+              sx={{ marginTop: "20px", maxWidth: "300px", mb: "100px" }}
+            >
+              Place Order
+            </OrangeButton>
+          </Box>
+        </BgOverlay>
+      </WorksMainHero>
+      <PlatformContainer container>
+        <PLatformItem item xs={12} md={6}>
+          <Typography
+            sx={{
+              color: "#ffff",
+              fontWeight: "900",
+              fontSize: "60px",
+              width: "65%",
+              margin: "auto",
+              mt: "100px",
+              lineHeight: "72px",
+              letterSpacing: "-3px",
+            }}
+          >
+            How our platform Works{" "}
+          </Typography>
+          <br />
+          <br />
+          <WhiteBox />
+        </PLatformItem>
+        <PLatformItem item xs={12} md={6}>
+          <Grid container sx={{ marginTop: "150px" }}>
+            <Grid item md={3} sx={{ textAlign: "center" }}>
+              <img src="./assets/SearchOrange.svg" style={{ width: "100px" }} />
+              <br />
+              <br />
+
+              <img src="./assets/line.svg" style={{ height: "100px" }} />
+            </Grid>
+            <Grid item md={9}>
+              <UnparalleledMainText sx={{ color: "#fff" }}>
+                Place order
+              </UnparalleledMainText>
+              <br />
+              <UnparalleledSubText sx={{ maxWidth: "500px", color: "#E0D2A4" }}>
+                Place an order via Whatsapp or our Webstore.
+                <br />
+                <br /> Supplier details, contracts, and quality assurance
+                information in a transparent and tamper-proof manner are
+                recorded in the blockchain. <br />
+                <br /> Contracts with suppliers would typically outline terms
+                such as feed quality, pricing, delivery schedules, and payment
+                terms.
+              </UnparalleledSubText>
+            </Grid>
+          </Grid>
+          <Grid container sx={{ marginTop: "60px" }}>
+            <Grid item md={3} sx={{ textAlign: "center" }}>
+              <img
+                src="./assets/OrangeContract.svg"
+                style={{ width: "100px" }}
+              />
+              <br />
+              <br />
+
+              <img src="./assets/line.svg" style={{ height: "100px" }} />
+            </Grid>
+            <Grid item md={9}>
+              <UnparalleledMainText sx={{ color: "#fff" }}>
+                Order processing
+              </UnparalleledMainText>
+              <br />
+              <UnparalleledSubText sx={{ maxWidth: "500px", color: "#E0D2A4" }}>
+                We implement Smart logistics contracts on the XDC blockchain to
+                facilitate seamless transportation arrangements and optimize
+                routes, reducing transportation costs.
+              </UnparalleledSubText>
+            </Grid>
+          </Grid>
+        </PLatformItem>
+        <PLatformItem
+          item
+          xs={12}
+          md={6}
+          sx={{ background: "#fff", pb: "100px" }}
+        >
+          <WhiteBox
+            sx={{
+              borderTopRightRadius: "0",
+              height: "600px",
+              boxShadow: "rgba(0, 0, 0, 0.3) 0px 26px 26px",
+              borderTop: "1px solid #fff",
+            }}
+          >
+            <Box sx={{ mt: "-200px", width: "100%" }}>
+              <img src="./assets/Stock.png" style={{ width: "100%" }} />
+            </Box>
+          </WhiteBox>
+        </PLatformItem>
+        <PLatformItem
+          item
+          xs={12}
+          md={6}
+          sx={{ background: "#fff", pb: "100px" }}
+        >
+          <Typography
+            sx={{
+              color: "#000",
+              fontWeight: "900",
+              fontSize: "60px",
+              width: "80%",
+              margin: "auto",
+              mt: "100px",
+              lineHeight: "65px",
+              letterSpacing: "-3px",
+            }}
+          >
+            We believe that in a world that{" "}
+            <span style={{ color: "#DFBC6D" }}>
+              {" "}
+              turns you into a business, we are able to use technology
+            </span>{" "}
+            to deliver to you an unparalleled dairy feeds supply.
+          </Typography>
+        </PLatformItem>
+      </PlatformContainer>
+      <Part>
+        <Box>
+        <Typography
+          sx={{
+            color: "#ffff",
+            fontWeight: "900",
+            fontSize: "60px",
+            margin: "auto",
+            lineHeight: "",
+            letterSpacing: "-3px",
+          }}
+        >
+          They were Part, they say...
+        </Typography>
         </Box>
-      </BgOverlay>
-    </WorksMainHero>
-    <Footer />
+      </Part>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default HotItWorks;
