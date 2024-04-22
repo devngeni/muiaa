368bebdf02ba7429a1edd61b95126a2b52d987cd
collinskoech11 <collinskoechck34@gmail.com>
2024-04-22 16:53:42 +0300

    delay redir

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import DefaultLayout from '@/components/layouts/defaultLayout';
-import React, { ReactElement, useEffect, useState } from 'react';
+import DefaultLayout from "@/components/layouts/defaultLayout";
+import React, { ReactElement, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Home = () => {
   const router = useRouter();
@@ -11,10 +11,13 @@ const Home = () => {
     const { token }: any = router.query;
     sessionStorage.setItem("auth_token", token);
     console.log(sessionStorage.getItem("auth_token"));
-
-    if(token === undefined) {
-      router.push("https://muiafeeds.com");
-      console.log("token not found");
+    if (token === undefined) {
+      setTimeout(() => {
+        if (token === undefined) {
+          console.log("token not found");
+          router.push("https://muiaafeeds.com");
+        }
+      }, 2000);
     } else if (token) {
       setTimeout(() => {
         router.push("/home");
@@ -29,7 +32,14 @@ const Home = () => {
   return (
     <>
       {loading ? ( // Render loader if loading state is true
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
           <CircularProgress />
         </div>
       ) : null}
