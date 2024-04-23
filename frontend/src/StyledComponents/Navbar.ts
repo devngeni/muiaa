1f586539fbfb95b7712453a784b5c6f24d1d85b9
Raziur306 <raziur.rahaman.ronju@gmail.com>
2024-04-23 12:36:36 +0600

    Responsive Typography fix

--- frontend/src/StyledComponents/Navbar.ts
+++ frontend/src/StyledComponents/Navbar.ts
@@ -1,37 +1,50 @@
 import { Box, Button, Grid, styled } from "@mui/material";
 
 export const NavbarContainer = styled(Box)({
-    width:"100vw",
-    height:"100px",
-    backgroundColor:"#14263E",
-})
+  width: "100vw",
+  height: "100px",
+  backgroundColor: "#14263E",
+});
 export const NavbarMiniContainer = styled(Grid)({
-    width:"90%",
-    margin:"auto",
-    justifyContent:"space-between",
-    alignItems:"center",
-    height:"100%",
-})
-export const NavbarLogoItem  = styled(Grid)({
-
-})
+  width: "90%",
+  margin: "auto",
+  justifyContent: "space-between",
+  alignItems: "center",
+  height: "100%",
+});
+export const NavbarLogoItem = styled(Grid)({
+  "@media screen and (max-width:890px)": {
+    overflow: "hidden",
+    height: "100%",
+    maxHeight: "100px",
+    img: {
+      height: "100%",
+    },
+  },
+  "@media screen and (max-width:600px)": {
+    height: "auto",
+    img: {
+      height: "100%",
+    },
+  },
+});
 export const NavbarLinksContainer = styled(Grid)({
-    display:"flex",
-    flexDirection:"row",
-    justifyContent:"space-around",
-    alignItems:"center",
-    height:"100%",
-    width:"30%",
-})
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+  height: "100%",
+  width: "30%",
+});
 
 export const NavBarButtonsContainer = styled(Grid)({
-    width:"20%",
-    display:"flex",
-    flexDirection:"row",
-    justifyContent:"space-around",
-    alignItems:"center",
-    height:"100%",
-})
+  width: "20%",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+  height: "100%",
+});
 
 export const NavButton = styled(Button)({
   border: "1px solid white",
@@ -40,16 +53,16 @@ export const NavButton = styled(Button)({
   width: "40%",
   height: "40px",
   fontSize: "10px",
-  fontWeight:"900",
+  fontWeight: "900",
   "&:hover": {
     background: "#E9BC6D",
     color: "#003565",
-    border:"none",
+    border: "none",
   },
 });
 export const NavButton2 = styled(NavButton)({
-    color: "#DFBC6D",
-    border: "2px solid #DFBC6D",
-    width:"40%",
-    textAlign:"center",
+  color: "#DFBC6D",
+  border: "2px solid #DFBC6D",
+  width: "40%",
+  textAlign: "center",
 });
